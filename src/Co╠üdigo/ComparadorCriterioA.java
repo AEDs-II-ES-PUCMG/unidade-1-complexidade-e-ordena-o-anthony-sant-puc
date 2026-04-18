9e7705229261f44f80b7cd30bb5bc89566692af7
Anthony Santos <anthony.carvalho@sga.pucminas.br>
2026-04-17 22:28:39 -0300

    Implement main application logic and data structures for order management system

---
+++ src/Co╠üdigo/ComparadorCriterioA.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+/**
+ * Critério A - Valor Final do Pedido (crescente).
+ * Desempate 1: Volume Total de Itens (quantProdutos).
+ * Desempate 2: Código Identificador do primeiro item do pedido.
+ */
+public class ComparadorCriterioA implements Comparator<Pedido> {
+
+    @Override
+    public int compare(Pedido o1, Pedido o2) {
+        ComparadorPorValor comp = new ComparadorPorValor();
+        return comp.compare(o1, o2);
+    }
+}
